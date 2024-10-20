8c0dd2fc379ca70e288ea52f4bf5ffc88289acc5
Putthipong Soongsuwan <putthipong.sowork@gmail.com>
2024-10-21 03:36:53 +0700

    I'am finsih Page[[Start]] 50% and I add +img/p01-p039 +img/GO/10faculty

--- my-tutpc/src/routes/+page.svelte
+++ my-tutpc/src/routes/+page.svelte
@@ -1,40 +1,43 @@
 <script>
-	import "../app.css";
+	let page = 1;
+	let point = 0;
+	let imgWidth = 0;
+	let imgHeight = 0;
+
+	import P01 from './nested-components/p01.svelte';
+
+	function handleImageLoad(event) {
+		imgWidth = event.target.naturalWidth;
+		imgHeight = event.target.naturalHeight;
+	}
+
+	function handleNextPage() {
+		page += 1;
+	}
 </script>
 
-
 <body>
-
-	<div class="block">
-		<h1 class="logo">What is <br>your <span style="font-size: 2.4em; font-style: italic; font-weight: bold;">Toy?</span></h1>
-		<h3 class="description">Learn what powers you possess</h3>
-		<button type="button" class="text-white bg-gradient-to-r from-purple-700 to-pink-300 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start</button>
-	</div>
-
+	<img src="./Img/{page}.png" alt="P{page}" on:load={handleImageLoad}>
+	<P01 page={page} point={point} imgHeight={imgHeight} imgWidth={imgWidth} on:nextPage={handleNextPage}/>
 </body>
 
 <style>
-	body {
-		width: 100%;
-		height: 100vh;
-		margin: 0;
-		padding: 0;
-		background-color: white;
-		display: grid;
-		place-items: center;
-	}
-	.block {
-		display: grid;
-		justify-items: center;
-		align-items: center;
-	}
-	.logo {
-		font-size: 2em;
-		font-weight: bold;
-	}
-	.description {
-		font-size: 1em;
-		margin-bottom: 20px;
-	}
+	
+body {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	min-height: 100vh;
+	margin: 0;
+}
 
+img {
+	position: absolute;
+	z-index: 0;
+	max-width: 100vw;
+	max-height: 100vh;
+	width: auto;
+	height: auto;
+	object-fit: contain;
+}
 </style>
---
+++ my-tutpc/src/routes/nested-components/p01.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+	export let page;
+	export let point;
+	export let imgWidth;
+	export let imgHeight;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('nextPage');
+	}
+</script>
+		<div class="space"></div>
+		<button on:click={handleClick}>Start</button>
+<style>
+</style>
