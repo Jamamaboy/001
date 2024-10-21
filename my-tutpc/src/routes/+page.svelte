d3900d93e80a1d7b8fd57704cbf1b787c8ab8218
Putthipong Soongsuwan <putthipong.sowork@gmail.com>
2024-10-21 12:18:08 +0700

    Finished doing only the categories on +page.svelte { Start TBC NextX2 NextX3 N S L NestP ENG }

--- my-tutpc/src/routes/+page.svelte
+++ my-tutpc/src/routes/+page.svelte
@@ -3,26 +3,61 @@
 	let point = 0;
 	let imgWidth = 0;
 	let imgHeight = 0;
+	let contentHeight = 0;
 
-	import P01 from './nested-components/p01.svelte';
+	import PStart from './nested-components/pStart.svelte';
+
 
 	function handleImageLoad(event) {
 		imgWidth = event.target.naturalWidth;
 		imgHeight = event.target.naturalHeight;
+
+		// คำนวณความสูงของเนื้อหาโดยใช้สัดส่วนของภาพ
+		contentHeight = (imgHeight / imgWidth) * Math.min(window.innerWidth, imgWidth);
 	}
 
 	function handleNextPage() {
 		page += 1;
+		console.log(page);
 	}
+
 </script>
 
 <body>
 	<img src="./Img/{page}.png" alt="P{page}" on:load={handleImageLoad}>
-	<P01 page={page} point={point} imgHeight={imgHeight} imgWidth={imgWidth} on:nextPage={handleNextPage}/>
+	{#if page == 1}
+	<!-- Start -->
+		<PStart page={page} point={point} imgHeight={imgHeight} imgWidth={imgWidth} on:nextPage={handleNextPage}/>
+
+	{:else if page == 2}
+	<!-- TBC -->
+
+	{:else if page == 3}
+	<!-- NextX2 -->
+
+	{:else if page == 3}
+	<!-- NextX3 -->
+
+	{:else if page == 3}
+	<!-- N -->
+
+	{:else if page == 15}
+	<!-- S -->
+
+	{:else if page == 3}
+	<!-- L -->
+
+	{:else if NestP.includes(page)}
+	<!-- NestP -->
+
+	{:else if page == 40}
+	<!-- ENG -->
+
+	{/if}
 </body>
 
 <style>
-	
+
 body {
 	display: flex;
 	justify-content: center;
---
+++ my-tutpc/src/routes/nested-components/pStart.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+	export let page;
+	export let point;
+	export let imgWidth;
+	export let imgHeight;
+	export let contentHeight;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('nextPage');
+	}
+
+
+</script>
+
+<div class="content" style="height: {contentHeight}px;">
+	<div class="space"></div>
+	<button on:click={handleClick}>Start</button>
+</div>
+
+<style>
+	.content {
+		display: grid;
+		z-index: 1;
+		grid-template-columns: 1fr;
+		grid-template-rows: 20fr 1fr;
+	}
+	.space {
+		height: 100%;
+	}
+</style>
